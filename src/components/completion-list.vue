cc51c3594dc6af60b86da35e1cf573bd610d0b2e
Hartaithan. <hartaithan@gmail.com>
2025-10-08 16:09:22 +0900

    add submit form and completion list components

---
+++ src/components/completion-list.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useCompletionStore } from "../store/completion";
+
+const store = useCompletionStore();
+</script>
+
+<template>
+  <div class="container mt-6">
+    <div v-if="store.list.length > 0" class="flex flex-col items-center justify-center gap-2 p-12">
+      <p v-for="item in store.list" :key="item.title">
+        {{ item.title }}
+      </p>
+    </div>
+    <p v-else class="text-center">nothing found :(</p>
+  </div>
+</template>
---
+++ src/components/submit-form.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupInput } from "@/ui/input-group";
+import { SendHorizonal } from "lucide-vue-next";
+
+interface Elements extends HTMLFormControlsCollection {
+  id: { value: string };
+}
+
+interface From extends HTMLFormElement {
+  elements: Elements;
+}
+
+const handleSubmit: (event: SubmitEvent) => void = (event) => {
+  const { elements } = event.target as From;
+  const id = elements?.id.value.trim();
+  console.info("id", id);
+};
+</script>
+
+<template>
+  <form class="container" @submit.prevent="handleSubmit">
+    <InputGroup>
+      <InputGroupInput name="id" placeholder="Enter your PSN ID" />
+      <InputGroupAddon align="inline-end">
+        <InputGroupButton size="icon-xs" type="submit">
+          <SendHorizonal />
+        </InputGroupButton>
+      </InputGroupAddon>
+    </InputGroup>
+  </form>
+</template>
