630ea343b02f0063df758cf227833da86b773f9b
Hartaithan. <hartaithan@gmail.com>
2025-10-09 20:52:38 +0900

    run dev server on https

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
+import ssl from "@vitejs/plugin-basic-ssl";
 import vue from "@vitejs/plugin-vue";
 import path from "node:path";
 import { defineConfig } from "vite";
@@ -6,7 +7,11 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss(), ssl()],
+  server: {
+    host: "local.hartaithan.com",
+    port: 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
