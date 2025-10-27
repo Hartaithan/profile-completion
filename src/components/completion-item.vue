736f838ff7530233b9847839f976df6b917c550e
Hartaithan. <hartaithan@gmail.com>
2025-10-27 17:19:40 +0900

    add trophy counts and platform

--- src/components/completion-item.vue
+++ src/components/completion-item.vue
@@ -7,6 +7,7 @@ import { getTrophiesProgress } from "@/utils/trophies";
 import { CircleCheckIcon, CircleDashedIcon } from "lucide-vue-next";
 import { computed } from "vue";
 import GameImage from "./game-image.vue";
+import TrophyCounts from "./trophy-counts.vue";
 
 interface Props {
   completion: Completion | null;
@@ -28,19 +29,20 @@ const trophies = computed(() =>
   <div class="flex w-full items-center" :class="{ 'opacity-50 grayscale': trophies?.completed }">
     <GameImage :src="completion?.image_url" :alt="completion?.title" />
     <div class="ml-3 flex flex-col justify-center">
-      <h1 class="font-bold">{{ completion?.title }}</h1>
-      <div class="flex gap-x-2">
-        <p class="text-sm">
-          Progress:
-          <span class="font-semibold">{{ formatProgress(completion?.progress, "%") }}</span>
-        </p>
-        <p class="text-sm">
-          Trophies:
-          <span class="font-semibold" v-if="trophies">
-            {{ `${trophies.earned}/${trophies.total}` }}
-          </span>
-          <span class="font-semibold" v-else>Not Found</span>
-        </p>
+      <p class="text-md font-bold">
+        {{ completion?.title }}
+        <span class="ml-1 align-text-bottom text-xs font-medium">{{
+          completion?.platforms.join(", ")
+        }}</span>
+      </p>
+      <div class="mt-0.5 flex gap-x-3 text-sm font-semibold">
+        <span>{{ formatProgress(completion?.progress, "%") }}</span>
+        <span class="trophy-dot" data-type="total" v-if="trophies">
+          {{ `${trophies.earned}/${trophies.total}` }}
+        </span>
+        <div class="flex gap-x-3">
+          <TrophyCounts :counts="completion?.counts" :earned="completion?.earned_counts" />
+        </div>
       </div>
     </div>
     <Button class="ml-auto" variant="outline" size="icon" @click="store.completeItem(index)">
---
+++ src/components/trophy-counts.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { TrophyCounts } from "@/models/trophy";
+
+interface Props {
+  counts: TrophyCounts | undefined;
+  earned: TrophyCounts | undefined;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <span v-if="counts?.platinum" :class="['trophy-dot', 'trophy-platinum']" data-type="platinum">{{
+    `${earned?.platinum}/${counts?.platinum}`
+  }}</span>
+  <span v-if="counts?.gold" :class="['trophy-dot', 'trophy-gold']" data-type="gold">{{
+    `${earned?.gold}/${counts?.gold}`
+  }}</span>
+  <span v-if="counts?.silver" :class="['trophy-dot', 'trophy-silver']" data-type="silver">{{
+    `${earned?.silver}/${counts?.silver}`
+  }}</span>
+  <span v-if="counts?.bronze" :class="['trophy-dot', 'trophy-bronze']" data-type="bronze">{{
+    `${earned?.bronze}/${counts?.bronze}`
+  }}</span>
+</template>
