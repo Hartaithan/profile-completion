c50a4f194677b10f99fd042ea055c299a572c282
Hartaithan. <hartaithan@gmail.com>
2025-12-31 14:00:29 +0900

    responsive layout for profile

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -7,24 +7,29 @@ const store = useCompletionStore();
 </script>
 
 <template>
-  <div v-if="store.loading" class="container mt-6 flex w-full items-center gap-4">
-    <Skeleton class="size-12 rounded-full" />
+  <div
+    v-if="store.loading"
+    class="container mt-6 flex w-full flex-wrap items-center gap-4 md:flex-nowrap">
+    <Skeleton class="aspect-square size-12 min-w-12 rounded-full" />
     <div className="flex flex-col gap-y-2">
       <Skeleton class="h-4 w-32" />
       <Skeleton class="h-3 w-24" />
     </div>
-    <Skeleton class="ml-auto h-6 w-80" />
+    <Skeleton class="mx-auto h-6 w-80 sm:mx-0 sm:ml-auto" />
   </div>
-  <div v-if="!store.loading && store.profile" class="container mt-6 flex w-full items-center gap-4">
+  <div
+    v-if="!store.loading && store.profile"
+    class="container mt-6 flex w-full flex-wrap items-center gap-4 md:flex-nowrap">
     <img
-      class="size-12"
+      class="aspect-square size-12 min-w-12"
       :src="store.profile?.avatar_url"
       :alt="`${store.profile?.name}'s avatar`" />
     <div className="flex flex-col leading-[normal]">
       <h1 className="font-bold">{{ store.profile?.name ?? "Trophy Hunter" }}</h1>
       <p className="text-sm text-gray-300">Level: {{ store.profile?.level ?? "420" }}</p>
     </div>
-    <div class="ml-auto flex gap-3 font-bold">
+    <div
+      class="mx-auto flex flex-wrap justify-center gap-3 font-bold sm:mx-0 sm:ml-auto sm:flex-nowrap sm:justify-start">
       <TrophyCounts :counts="store.profile?.counts" :earned="undefined" />
     </div>
   </div>
