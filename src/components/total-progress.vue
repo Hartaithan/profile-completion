49d6d1376f7fb31dbd80eca72e309303a11c6265
Hartaithan. <hartaithan@gmail.com>
2026-04-14 16:17:21 +0900

    update total progress layout

--- src/components/total-progress.vue
+++ src/components/total-progress.vue
@@ -8,24 +8,32 @@ const store = useCompletionStore();
 </script>
 
 <template>
-  <div v-if="store.loading" class="container py-6 text-center">
-    <Skeleton class="h-5 w-full sm:h-7" />
-    <Skeleton class="mt-2 h-7 w-full sm:h-9" />
+  <div v-if="store.loading" class="container py-6">
+    <Skeleton class="h-40 w-full" />
   </div>
-  <div
-    v-else-if="store.profile"
-    class="from-background via-background sticky top-0 z-50 container flex flex-col bg-gradient-to-b from-0% via-90% to-transparent to-100% py-6">
-    <span class="text-sm font-bold sm:text-xl">
-      Total Completion: {{ formatProgress(store.calculated.progress.progress, "%") }}
-    </span>
-    <div class="mt-2">
-      <div class="flex justify-between">
-        <p class="text-xs sm:text-base">Points</p>
-        <p class="text-xs font-bold sm:text-base">
-          {{ `${store.calculated.progress.earned} / ${store.calculated.progress.points}` }}
-        </p>
+  <div v-else-if="store.profile" class="sticky top-0 z-50 container py-6">
+    <div
+      class="bg-card/50 border-border/50 flex h-40 flex-col justify-center rounded-lg border p-6">
+      <div className="flex justify-between items-end">
+        <div className="flex flex-col">
+          <span className="text-xs font-bold uppercase tracking-widest text-muted-foreground">
+            Completion Status
+          </span>
+          <h2 className="text-2xl font-black tracking-tight text-foreground uppercase">
+            Total Completion: {{ formatProgress(store.calculated.progress.progress, "%") }}
+          </h2>
+        </div>
+        <span className="text-sm font-mono text-primary uppercase"
+          >{{ store.calculated.progress.points }} points</span
+        >
       </div>
-      <Progress class="mt-1" :model-value="getProgress(store.calculated.progress)" />
+      <Progress class="mt-4" :model-value="getProgress(store.calculated.progress)" />
+      <div className="mt-4 flex items-center gap-2">
+        <span className="w-1 h-4 bg-primary/50" />
+        <span className="text-xs font-bold uppercase tracking-widest text-muted-foreground">
+          Games Tracked: {{ store.calculated.completion.length }}
+        </span>
+      </div>
     </div>
   </div>
 </template>
