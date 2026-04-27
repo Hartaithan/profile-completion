496bdc9919647496243713007ebcef4718d388f3
Hartaithan. <hartaithan@gmail.com>
2026-04-27 17:10:04 +0900

    update trophy counts layout

--- src/components/profile-trophy-counts.vue
+++ src/components/profile-trophy-counts.vue
@@ -9,41 +9,42 @@ const store = useCompletionStore();
 
 <template>
   <div v-if="store.status === 'profile-loading'" class="container mt-6 grid grid-cols-5 gap-2">
-    <Skeleton class="h-24" />
-    <Skeleton class="h-24" />
-    <Skeleton class="h-24" />
-    <Skeleton class="h-24" />
-    <Skeleton class="h-24" />
+    <Skeleton class="h-20" />
+    <Skeleton class="h-20" />
+    <Skeleton class="h-20" />
+    <Skeleton class="h-20" />
+    <Skeleton class="h-20" />
   </div>
   <div
     v-if="store.status !== 'profile-loading' && store.profile"
     class="container mt-6 grid grid-cols-5 gap-2">
     <ProfileTrophyStatItem
       label="Platinum"
-      valueClass="text-trophy-platinum"
-      :value="store.profile.counts.platinum">
-      <AwardIcon class="text-foreground/90 size-6" />
-    </ProfileTrophyStatItem>
+      value-class="text-trophy-platinum"
+      icon-class="text-trophy-platinum"
+      :value="store.profile.counts.platinum"
+      :icon="AwardIcon" />
     <ProfileTrophyStatItem
       label="Gold"
-      valueClass="text-trophy-gold"
-      :value="store.profile.counts.gold">
-      <TrophyIcon class="text-foreground/90 size-6" />
-    </ProfileTrophyStatItem>
+      value-class="text-trophy-gold"
+      icon-class="text-trophy-gold"
+      :value="store.profile.counts.gold"
+      :icon="TrophyIcon" />
     <ProfileTrophyStatItem
       label="Silver"
-      valueClass="text-trophy-silver"
-      :value="store.profile.counts.silver">
-      <TrophyIcon class="text-foreground/90 size-6" />
-    </ProfileTrophyStatItem>
+      value-class="text-trophy-silver"
+      icon-class="text-trophy-silver"
+      :value="store.profile.counts.silver"
+      :icon="TrophyIcon" />
     <ProfileTrophyStatItem
       label="Bronze"
-      valueClass="text-trophy-bronze"
-      :value="store.profile.counts.bronze">
-      <TrophyIcon class="text-foreground/90 size-6" />
-    </ProfileTrophyStatItem>
-    <ProfileTrophyStatItem label="Total" :value="store.profile.counts.total">
-      <BarChart3Icon class="text-foreground/90 size-6" />
-    </ProfileTrophyStatItem>
+      value-class="text-trophy-bronze"
+      icon-class="text-trophy-bronze"
+      :value="store.profile.counts.bronze"
+      :icon="TrophyIcon" />
+    <ProfileTrophyStatItem
+      label="Total"
+      :value="store.profile.counts.total"
+      :icon="BarChart3Icon" />
   </div>
 </template>
--- src/components/profile-trophy-stat-item.vue
+++ src/components/profile-trophy-stat-item.vue
@@ -1,23 +1,28 @@
 <script setup lang="ts">
+import type { Component } from "vue";
+
 interface Props {
   label: string;
   value: string | number | undefined;
   labelClass?: string;
   valueClass?: string;
+  iconClass?: string;
+  icon?: Component;
 }
 
 defineProps<Props>();
 </script>
 
 <template>
-  <div
-    class="bg-card border-border/50 flex h-24 flex-col items-center justify-center rounded-lg border p-2">
-    <slot />
-    <span :class="['mt-1 text-lg font-black', valueClass]">
-      {{ value ?? "-" }}
-    </span>
-    <span :class="['text-muted-foreground text-xxs font-bold uppercase', labelClass]">
-      {{ label }}
-    </span>
+  <div class="bg-card border-border/50 flex h-20 items-center justify-center rounded-lg border">
+    <component :is="icon" v-if="icon" :class="['text-foreground/90 size-8', iconClass]" />
+    <div class="ml-3 flex flex-col">
+      <span :class="['text-2xl font-black', valueClass]">
+        {{ value ?? "-" }}
+      </span>
+      <span :class="['text-muted-foreground text-xs font-bold uppercase', labelClass]">
+        {{ label }}
+      </span>
+    </div>
   </div>
 </template>
